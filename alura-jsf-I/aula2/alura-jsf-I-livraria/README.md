46a2cf9396154a2ebd0cad75e1e23cb27b281a92
jkavdev <jhonatankolen@gmail.com>
2017-04-23 02:02:16 -0300

    Java e JSF I- Aula 2 - Atividade 1 Vídeo 1 - Criando projeto livraria do curso, criando formulário e vinculando ao model Livro, que é gerenciado pelo bean LivroBean

---
+++ alura-jsf-I/aula2/alura-jsf-I-livraria/README.md
@@ -0,0 +1,6 @@
+Projeto Livraria do Curso de Jsf do Alura
+
+Dependências usadas
+	
+	javax.faces-2.1.14.jar
+	
---
+++ alura-jsf-I/aula2/alura-jsf-I-livraria/src/br/com/jkavdev/alura/jsf/livraria/bean/LivroBean.java
@@ -0,0 +1,22 @@
+package br.com.jkavdev.alura.jsf.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public void gravar() {
+		System.out.println("Gravando livro: " + livro.getTitulo());
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+}
