d6ac222c3c5df3c2d8c3367f600a4a2f39c06346
jkavdev <jhonatankolen@gmail.com>
2017-05-16 16:33:29 -0300

    EJB- Aula 8 - Atividade 7 - Adicionando agendamento a cada 5 segundos

--- alura-ejb/alura-ejb-timerservice/ejbModule/br/com/jkavdev/alura/ejb/timer/Agendador.java
+++ alura-ejb/alura-ejb-timerservice/ejbModule/br/com/jkavdev/alura/ejb/timer/Agendador.java
@@ -8,7 +8,12 @@ import javax.ejb.Startup;
 @Startup
 public class Agendador {
 	
-	@Schedule(hour="*", minute="*", second="*/10", persistent=true)
+	@Schedule(hour="*", minute="*", second="*/5", persistent=false)
+	void aCada2Minutos(){
+		System.out.println("Verificando recurso externo a cada 2 min......");
+	}
+	
+	@Schedule(hour="*", minute="*", second="*/10", persistent=false)
 	void agendado(){
 		System.out.println("Verificando recurso externo......");
 	}
