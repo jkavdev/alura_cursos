db9bff0a038efdba1203e39afbdd85b92e41a1df
jkavdev <jhonatankolen@gmail.com>
2017-10-05 17:48:11 -0300

    Java EE- Aula 2 - Atividade 1 Criando a relação de Livro com Autor
    - Criando a classe autor
    - Exibindo os autores na tela de cadastro de livro

--- alura-javaee/alura-javaee-casadocodigo/src/main/java/br/com/jkavdev/alura/javaee/casadocodigo/beans/AdminLivrosBean.java
+++ alura-javaee/alura-javaee-casadocodigo/src/main/java/br/com/jkavdev/alura/javaee/casadocodigo/beans/AdminLivrosBean.java
@@ -1,11 +1,15 @@
 package br.com.jkavdev.alura.javaee.casadocodigo.beans;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import br.com.jkavdev.alura.javaee.casadocodigo.daos.LivroDao;
+import br.com.jkavdev.alura.javaee.casadocodigo.models.Autor;
 import br.com.jkavdev.alura.javaee.casadocodigo.models.Livro;
 
 //Utilizando anotacoes do cdi
@@ -23,6 +27,11 @@ public class AdminLivrosBean {
 	public void salvar() {
 		dao.salvar(livro);
 		System.out.println("Livro salvo com sucesso");
+	}
+	
+	//criando uma lista de autores, por enquanto na mao mesmo
+	public List<Autor> getAutores(){
+		return Arrays.asList(new Autor(1, "Jhonatan"), new Autor(1, "Lucas"));
 	}
 
 	public Livro getLivro() {
