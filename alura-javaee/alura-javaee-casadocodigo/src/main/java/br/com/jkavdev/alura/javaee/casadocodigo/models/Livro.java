359d24c8454b3f2975e14b920002b087fe372f3e
jkavdev <jhonatankolen@gmail.com>
2017-10-06 13:24:19 -0300

    Java EE- Aula 3 - Atividade 9 Usando a Bean Validation
    - Retirando as validações do JSF na página de cadastro de livro
    - Utilizando o bean validation para realizar a validação dos dados
    - Adicionando o h:message para cada atributo na página
    - Redefinindo mensagem padrão do bean validation no jsf_messages.properties

--- alura-javaee/alura-javaee-casadocodigo/src/main/java/br/com/jkavdev/alura/javaee/casadocodigo/models/Livro.java
+++ alura-javaee/alura-javaee-casadocodigo/src/main/java/br/com/jkavdev/alura/javaee/casadocodigo/models/Livro.java
@@ -10,6 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 //Mapeando para uma entidade 
 @Entity
@@ -18,16 +25,29 @@ public class Livro {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	//Utilizando o bean validation para validar os dados dos atributos
+	//Estamos informando que esta campo nao aceita valor vazio, 
+	@NotBlank
 	private String titulo;
 
+	//Nao aceita espacos em branco e o tamanho minimo eh 10
+	@NotBlank
+	@Length(min = 10)
 	// Para dados muitos grandes
 	@Lob
 	private String descricao;
 
+	//Validacao para decimais, menor numero a ser informado eh 20
+	@DecimalMin("20")
 	private BigDecimal preco;
 
+	//Validacao para inteiros, menor numero a ser informado eh 20
+	@Min(20)
 	private Integer numeroPaginas;
 
+	//validacao para autores, tem que selecionar pelo meno um autor
+	@NotNull
+	@Size(min = 1)
 	//livro tem varios autores
 	@ManyToMany
 	private List<Autor> autores = new ArrayList<>();
