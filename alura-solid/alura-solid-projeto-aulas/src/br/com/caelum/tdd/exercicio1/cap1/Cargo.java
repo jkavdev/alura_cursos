43d76d8bcf699f5486391ef37d66917e5e687fb1
jkavdev <jhonatankolen@gmail.com>
2017-10-19 13:54:03 -0200

    SOLID com Java- Aula 1 - Atividade 1 Vídeo 1
    - Criação do projeto do curso de SOLID

---
+++ alura-solid/alura-solid-projeto-aulas/src/br/com/caelum/tdd/exercicio1/cap1/Cargo.java
@@ -0,0 +1,7 @@
+package br.com.caelum.tdd.exercicio1.cap1;
+
+public enum Cargo {
+	DESENVOLVEDOR,
+	DBA,
+	TESTER
+}
---
+++ alura-solid/alura-solid-projeto-aulas/src/br/com/caelum/tdd/exercicio2/EnviadorDeEmail.java
@@ -0,0 +1,7 @@
+package br.com.caelum.tdd.exercicio2;
+
+
+public interface EnviadorDeEmail {
+
+	void enviaEmail(NotaFiscal nf);
+}
