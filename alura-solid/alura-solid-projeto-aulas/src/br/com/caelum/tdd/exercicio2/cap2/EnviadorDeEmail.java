509b4a2a95558ab823548f14976721889c1742cb
jkavdev <jhonatankolen@gmail.com>
2017-10-23 13:02:37 -0200

    SOLID com Java- Aula 2 - Atividade 1 Vídeo 1
    - Capítulo 2

---
+++ alura-solid/alura-solid-projeto-aulas/src/br/com/caelum/tdd/exercicio2/cap2/EnviadorDeEmail.java
@@ -0,0 +1,7 @@
+package br.com.caelum.tdd.exercicio2.cap2;
+
+
+public interface EnviadorDeEmail {
+
+	void enviaEmail(NotaFiscal nf);
+}
---
+++ alura-solid/alura-solid-projeto-aulas/src/br/com/caelum/tdd/exercicio2/cap2/Sap.java
@@ -0,0 +1,5 @@
+package br.com.caelum.tdd.exercicio2.cap2;
+
+public interface Sap {
+	public void envia(NotaFiscal nf);
+}
