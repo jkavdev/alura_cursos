3b7c9417e88fe2e06372a9675f36392d011c588d
jkavdev <jhonatankolen@gmail.com>
2018-05-01 02:41:47 -0300

    Collections - Aula 6 - Atividade 5 e 6 Collections.emptySet()

--- alura-collections/gerenciador-cursos/src/test/java/br/com/jkavdev/alura/collections/AlunoSetTest.java
+++ alura-collections/gerenciador-cursos/src/test/java/br/com/jkavdev/alura/collections/AlunoSetTest.java
@@ -6,6 +6,11 @@ import br.com.jkavdev.alura.collections.aula.Curso;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class AlunoSetTest {
 
     Curso curso;
@@ -30,6 +35,39 @@ public class AlunoSetTest {
         curso.matricula(brenda);
 
         System.out.println(curso.getAlunos());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void criaUmSetVazioImutavel(){
+        Aluno jhonatan = new Aluno("Jhonatan", 32154);
+        Set<Object> alunos = Collections.emptySet();
+        //cria um set vazio imutavel, no qual nao
+        //pode ocorrer alteracoes
+        //pode ter uma performance melhor que o new do set, pois o emptySet
+        //e mais leve do que um new set
+        alunos.add(jhonatan);
+    }
+
+    /**
+     * Uma das características mais interessantes de JVM é que ela sabe trabalhar em paralelo. Internamente isso é feito
+     * através de Threads que funcionam como pequenos processos dentro da JVM.
+     *
+     * O problema é que as coleções que estamos usando até agora não foram feitas para ser manipuladas em paralelo.
+     * No entanto, nada impede que usemos um método da classe Collections para transformar uma coleção comum em uma
+     * coleção para threads! É justamente isso que o método faz, retorna um nova coleção que pode ser compartilhada entre threads sem perigos.
+     */
+
+    @Test
+    public void criaUmSetThreadSafeTest(){
+        Aluno jhonatan = new Aluno("Jhonatan", 32154);
+        //por padrao um set nao eh thread safe
+        HashSet<Aluno> alunos = new HashSet<>();
+        alunos.add(jhonatan);
+        //podemos criar um com o collections
+        Set<Aluno> alunosSafe = Collections.synchronizedSet(alunos);
+        alunosSafe.add(jhonatan);
+        System.out.println(alunos);
+        System.out.println(alunosSafe);
     }
 
 }
