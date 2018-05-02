1480d524a34da4747b9ecd940a9ba9919f343b16
jkavdev <jhonatankolen@gmail.com>
2018-05-01 22:18:26 -0300

    Testes - Aula 2 - Atividade 12 Desafio - criando mais testes, e vendo como a implementacao de novas funcionalidades impactam nas existentes

--- alura-tdd/leilao-tdd/src/main/java/br/com/jkavdev/alura/tdd/leilao/servico/Avaliador.java
+++ alura-tdd/leilao-tdd/src/main/java/br/com/jkavdev/alura/tdd/leilao/servico/Avaliador.java
@@ -3,16 +3,30 @@ package br.com.jkavdev.alura.tdd.leilao.servico;
 import br.com.jkavdev.alura.tdd.leilao.dominio.Lance;
 import br.com.jkavdev.alura.tdd.leilao.dominio.Leilao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Avaliador {
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
 
     public void avalia(Leilao leilao) {
         for (Lance lance : leilao.getLances()) {
             if (lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
             if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
         }
+
+        osTresMaioresLances(leilao);
+    }
+
+    private void osTresMaioresLances(Leilao leilao) {
+        maiores = new ArrayList<>(leilao.getLances());
+        Collections.sort(maiores, Comparator.comparing(Lance::getValor).reversed());
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
     }
 
     public double getMaiorLance() {
@@ -23,4 +37,7 @@ public class Avaliador {
         return menorDeTodos;
     }
 
+    public List<Lance> getTresMaioresLances() {
+        return maiores;
+    }
 }
--- alura-tdd/leilao-tdd/src/test/java/br/com/jkavdev/alura/tdd/test/leilao/AvaliadorTest.java
+++ alura-tdd/leilao-tdd/src/test/java/br/com/jkavdev/alura/tdd/test/leilao/AvaliadorTest.java
@@ -7,6 +7,10 @@ import br.com.jkavdev.alura.tdd.leilao.servico.Avaliador;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class AvaliadorTest {
 
     /**
@@ -36,8 +40,61 @@ public class AvaliadorTest {
         //3 - Validar a saida
         double maiorLance = 5.8;
         double menorLance = 5.6;
-        Assert.assertEquals(maiorLance, leiloeiro.getMaiorLance(), 0.00001);
-        Assert.assertEquals(menorLance, leiloeiro.getMenorLance(), 0.00001);
+        assertEquals(maiorLance, leiloeiro.getMaiorLance(), 0.00001);
+        assertEquals(menorLance, leiloeiro.getMenorLance(), 0.00001);
+    }
+
+    /**
+     * Escreve Testes em Ordem de Equivalencia, ex:
+     * 1 - Lances em Ordem Crescente
+     * 2 - Lances em Ordem Decrescente
+     * 3 - Lances em Ordem Randomica
+     * 4 - Apenas um Lance
+     */
+
+    @Test
+    public void deveEntenderLeilaComUmLanceTest() {
+        //1 - Pensar no cenario
+        Usuario jhonatan = new Usuario("Jhonatan");
+
+        Leilao leilao = new Leilao("Sorvete da Bruna");
+
+        leilao.propoe(new Lance(jhonatan, 5.6));
+
+        //2 - Executar uma acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        //3 - Validar a saida
+        double maiorLance = 5.6;
+        double menorLance = 5.6;
+        assertEquals(maiorLance, leiloeiro.getMaiorLance(), 0.00001);
+        assertEquals(menorLance, leiloeiro.getMenorLance(), 0.00001);
+    }
+
+    @Test
+    public void deveEncontrarOsTresMaioresLancesTest() {
+        //1 - Pensar no cenario
+        Usuario jhonatan = new Usuario("Jhonatan");
+        Usuario douglas = new Usuario("Douglas");
+        Usuario lucas = new Usuario("Lucas");
+
+        Leilao leilao = new Leilao("Sorvete da Bruna");
+
+        leilao.propoe(new Lance(jhonatan, 5.6));
+        leilao.propoe(new Lance(douglas, 5.8));
+        leilao.propoe(new Lance(lucas, 5.7));
+
+        //2 - Executar uma acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        //3 - Validar a saida
+        List<Lance> maiores = leiloeiro.getTresMaioresLances();
+        assertEquals(3, maiores.size());
+        assertEquals(5.8, maiores.get(0).getValor(), 0.00001);
+        assertEquals(5.7, maiores.get(1).getValor(), 0.00001);
+        assertEquals(5.6, maiores.get(2).getValor(), 0.00001);
     }
 
 }
