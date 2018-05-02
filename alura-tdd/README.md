8a209a044b04fd2af68ba5f6d20d2db9c57d9018
jkavdev <jhonatankolen@gmail.com>
2018-05-01 21:27:07 -0300

    Testes - Aula 1 - Atividade 1 Vídeo 1 - criando projeto do curso

---
+++ alura-tdd/README.md
@@ -0,0 +1,1 @@
+# Testes - TDD com Java - Alura
---
+++ alura-tdd/leilao-tdd/src/main/java/br/com/jkavdev/alura/tdd/leilao/servico/Avaliador.java
@@ -0,0 +1,30 @@
+package br.com.jkavdev.alura.tdd.leilao.servico;
+
+import br.com.jkavdev.alura.tdd.leilao.dominio.Lance;
+import br.com.jkavdev.alura.tdd.leilao.dominio.Leilao;
+
+public class Avaliador {
+
+    private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+    public void avalia(Leilao leilao) {
+        int registros = leilao.getLances().size();
+        double soma = 0.0;
+        for (Lance lance : leilao.getLances()) {
+            if (lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+
+            soma += lance.getValor();
+        }
+    }
+
+    public double getMaiorLance() {
+        return maiorDeTodos;
+    }
+
+    public double getMenorLance() {
+        return menorDeTodos;
+    }
+
+}
